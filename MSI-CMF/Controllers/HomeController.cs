152dab2421d4462a08eb68ad7a9600d78d654890
mrivas-volcomcapital <mrivas@volcomcapital.com>
2026-03-23 10:17:26 -0300

    commit inicial

---
+++ MSI-CMF/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MSI_CMF.Models;
+using System.Diagnostics;
+
+namespace MSI_CMF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
